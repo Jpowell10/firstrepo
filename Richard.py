4a24b1c04178411eb5bb0a01c1d264ecd4e4dde9
Jackson <jpowell@shortridgeacademy.com>
2016-02-16 09:55:42 -0500

    common work flow

--- Richard.py
+++ Richard.py
@@ -1,6 +1,7 @@
-Richard = -5
-print(Richard)
-Steve = 5
-print(steve)
-blue = Richard + steve
-print(blue)
+List1 = [1, 2, 3, 4]
+List2 = ['I', 'tripped', 'over', 'and', 'hit', 'the', 'floor']
+print(List1 + List2)
+List3 = List1 + List2
+print(List3)
+fibs = (0, 1, 2, 3)
+print(fibs[3])
